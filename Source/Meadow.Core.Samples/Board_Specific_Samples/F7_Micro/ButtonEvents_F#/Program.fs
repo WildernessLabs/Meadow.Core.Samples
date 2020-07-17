84c405f232ce953a15fe07a9e530c3d39ec8c5de
Adrian Stevens <adrian.stevens@gmail.com>
2020-07-16 19:56:19 -0700

    Add F# samples for button events and walking digital outputs

---
+++ Source/Meadow.Core.Samples/Board_Specific_Samples/F7_Micro/ButtonEvents_F#/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open Meadow
+open Meadow.Devices
+open Meadow.Hardware
+open System.Threading
+
+
+type ButtonEventsApp() =
+    inherit App<F7Micro, ButtonEventsApp>()    
+          
+    let input = ButtonEventsApp.Device.CreateDigitalInputPort( ButtonEventsApp.Device.Pins.D02, InterruptMode.EdgeBoth, debounceDuration = (float 20))    
+    do input.Changed.Add(fun e -> 
+                            printfn "Changed %b Time:%s"  e.Value (e.New.ToString())
+                        )
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello from F#!"
+    let app = new ButtonEventsApp()
+    Thread.Sleep(Timeout.Infinite)
+    0 // return an integer exit code
