adbb46dbbded5a17a98a81dc9f71652f2e996564
bryan costanich <b@wowzer.net>
2020-09-16 22:18:18 -0700

    updated to catch exception

--- Source/Meadow.Core.Samples/Network/WiFi_Basics/MeadowApp.cs
+++ Source/Meadow.Core.Samples/Network/WiFi_Basics/MeadowApp.cs
@@ -7,6 +7,7 @@ using Meadow;
 using Meadow.Devices;
 using Meadow.Gateway.WiFi;
 using Meadow.Hardware;
+using System.IO;
 
 namespace WiFi_Basics
 {
@@ -16,22 +17,19 @@ namespace WiFi_Basics
         {
             Initialize();
 
-            //Device.WiFiAdapterInitilaized += (s,e) => {
 
-                Console.WriteLine($"Connecting to WiFi Network {Secrets.WIFI_NAME}");
+            Console.WriteLine($"Connecting to WiFi Network {Secrets.WIFI_NAME}");
 
-                if (Device.WiFiAdapter.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD).ConnectionStatus != ConnectionStatus.Success) {
-                    throw new Exception("Cannot connect to network, applicaiton halted.");
-                }
-                Console.WriteLine("Connection request completed.");
+            if (Device.WiFiAdapter.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD).ConnectionStatus != ConnectionStatus.Success) {
+                throw new Exception("Cannot connect to network, applicaiton halted.");
+            }
+            Console.WriteLine("Connection request completed.");
 
-                //throw new Exception("Test exception");
 
-                ScanForAccessPoints();
+            ScanForAccessPoints();
 
-                GetWebPageAsync("http://postman-echo.com/get?foo1=bar1&foo2=bar2").Wait();
+            GetWebPageViaHttpClient("http://postman-echo.com/get?foo1=bar1&foo2=bar2").Wait();
 
-            //};
         }
 
         void Initialize()
@@ -57,18 +55,25 @@ namespace WiFi_Basics
             }
         }
 
-        public async Task GetWebPageAsync(string uri)
+        public async Task GetWebPageViaHttpClient(string uri)
         {
             Console.WriteLine($"Requesting {uri}");
 
-            HttpClient client = new HttpClient();
-            client.Timeout = new TimeSpan(0, 5, 0);
+            using (HttpClient client = new HttpClient()) {
+                client.Timeout = new TimeSpan(0, 5, 0);
 
-            HttpResponseMessage response = await client.GetAsync(uri);
+                HttpResponseMessage response = await client.GetAsync(uri);
 
-            response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(responseBody);
+                try {
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                } catch (TaskCanceledException) {
+                    Console.WriteLine("Request time out.");
+                } catch (Exception e) {
+                    Console.WriteLine($"Request went sideways: {e.Message}");
+                }
+            }
         }
 
     }
