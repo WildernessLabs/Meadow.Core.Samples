7ef6364d3ee6be470d6dfcc0e3341e65b6ba1643
bryan costanich <b@wowzer.net>
2021-02-20 17:22:11 -0800

    async await cleanup

--- Source/Meadow.Core.Samples/Network/HttpListener/MeadowApp.cs
+++ Source/Meadow.Core.Samples/Network/HttpListener/MeadowApp.cs
@@ -65,48 +65,50 @@ namespace MeadowApp
         {
             bool runServer = true;
 
-            // While a user hasn't visited the `shutdown` url, keep on handling requests
-            while (runServer) {
-                // Will wait here until we hear from a connection
-                HttpListenerContext ctx = await listener.GetContextAsync();
+            await Task.Run(async () => {
+                // While a user hasn't visited the `shutdown` url, keep on handling requests
+                while (runServer) {
+                    // Will wait here until we hear from a connection
+                    HttpListenerContext ctx = await listener.GetContextAsync();
 
-                // Peel out the requests and response objects
-                HttpListenerRequest req = ctx.Request;
-                HttpListenerResponse resp = ctx.Response;
+                    // Peel out the requests and response objects
+                    HttpListenerRequest req = ctx.Request;
+                    HttpListenerResponse resp = ctx.Response;
 
-                // Print out some info about the request
-                Console.WriteLine("Request #: {0}", ++requestCount);
-                Console.WriteLine(req.Url);
-                Console.WriteLine(req.HttpMethod);
-                Console.WriteLine(req.UserHostName);
-                Console.WriteLine(req.UserAgent);
-                Console.WriteLine();
+                    // Print out some info about the request
+                    Console.WriteLine("Request #: {0}", ++requestCount);
+                    Console.WriteLine(req.Url);
+                    Console.WriteLine(req.HttpMethod);
+                    Console.WriteLine(req.UserHostName);
+                    Console.WriteLine(req.UserAgent);
+                    Console.WriteLine();
 
-                // If `shutdown` url requested w/ POST, then shutdown the server after serving the page
-                if ((req.HttpMethod == "POST") && (req.Url.AbsolutePath == "/shutdown")) {
-                    Console.WriteLine("Shutdown requested");
-                    runServer = false;
+                    // If `shutdown` url requested w/ POST, then shutdown the server after serving the page
+                    if ((req.HttpMethod == "POST") && (req.Url.AbsolutePath == "/shutdown")) {
+                        Console.WriteLine("Shutdown requested");
+                        runServer = false;
+                    }
+
+                    // Make sure we don't increment the page views counter if `favicon.ico` is requested
+                    if (req.Url.AbsolutePath != "/favicon.ico") {
+                        pageViews += 1;
+                    }
+
+                    // Write the response info
+                    string disableSubmit = !runServer ? "disabled" : "";
+                    byte[] data = Encoding.UTF8.GetBytes(String.Format(pageData, pageViews, disableSubmit));
+                    resp.ContentType = "text/html";
+                    resp.ContentEncoding = Encoding.UTF8;
+                    resp.ContentLength64 = data.LongLength;
+
+                    // Write out to the response stream (asynchronously), then close it
+                    await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                    resp.Close();
                 }
-
-                // Make sure we don't increment the page views counter if `favicon.ico` is requested
-                if (req.Url.AbsolutePath != "/favicon.ico") {
-                    pageViews += 1;
-                }
-
-                // Write the response info
-                string disableSubmit = !runServer ? "disabled" : "";
-                byte[] data = Encoding.UTF8.GetBytes(String.Format(pageData, pageViews, disableSubmit));
-                resp.ContentType = "text/html";
-                resp.ContentEncoding = Encoding.UTF8;
-                resp.ContentLength64 = data.LongLength;
-
-                // Write out to the response stream (asynchronously), then close it
-                await resp.OutputStream.WriteAsync(data, 0, data.Length);
-                resp.Close();
-            }
+            });
         }
 
-        public void StartServer()
+        public async void StartServer()
         {
             listener = new HttpListener();
             listener.Prefixes.Add(Url);
@@ -114,8 +116,7 @@ namespace MeadowApp
             Console.WriteLine("Listening for connections on {0}", Url);
 
             // Handle requests
-            Task listenTask = HandleIncomingConnections();
-            listenTask.GetAwaiter().GetResult();
+            await HandleIncomingConnections();
 
             // Close the listener
             listener.Close();
