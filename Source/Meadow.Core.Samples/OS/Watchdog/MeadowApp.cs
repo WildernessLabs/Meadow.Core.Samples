ccda87e8b01ec36b28b7e5239fd2ab0ddfc102e2
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-03-17 00:24:09 -0700

    Update reference on Watchdog project

--- Source/Meadow.Core.Samples/OS/Watchdog/MeadowApp.cs
+++ Source/Meadow.Core.Samples/OS/Watchdog/MeadowApp.cs
@@ -1,7 +1,5 @@
 ﻿using Meadow;
 using Meadow.Devices;
-using Meadow.Foundation;
-using Meadow.Foundation.Leds;
 using System;
 using System.Threading;
 
@@ -9,12 +7,9 @@ namespace MeadowApp
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
-        RgbPwmLed onboardLed;
-
         public MeadowApp()
         {
             Initialize();
-            CycleColors(1000);
         }
 
         void Initialize()
@@ -24,13 +19,6 @@ namespace MeadowApp
             // enable the watchdog for 10s
             MeadowOS.WatchdogEnable(10000);
             StartPettingWatchdog(9000);
-
-            onboardLed = new RgbPwmLed(device: Device,
-                redPwmPin: Device.Pins.OnboardLedRed,
-                greenPwmPin: Device.Pins.OnboardLedGreen,
-                bluePwmPin: Device.Pins.OnboardLedBlue,
-                3.3f, 3.3f, 3.3f,
-                Meadow.Peripherals.Leds.IRgbLed.CommonType.CommonAnode);
         }
 
         /// <summary>
@@ -50,41 +38,6 @@ namespace MeadowApp
                 }
             });
             t.Start();
-        }
-
-        void CycleColors(int duration)
-        {
-            Console.WriteLine("Cycle colors...");
-
-            while (true) {
-                ShowColorPulse(Color.Blue, duration);
-                ShowColorPulse(Color.Cyan, duration);
-                ShowColorPulse(Color.Green, duration);
-                ShowColorPulse(Color.GreenYellow, duration);
-                ShowColorPulse(Color.Yellow, duration);
-                ShowColorPulse(Color.Orange, duration);
-                ShowColorPulse(Color.OrangeRed, duration);
-                ShowColorPulse(Color.Red, duration);
-                ShowColorPulse(Color.MediumVioletRed, duration);
-                ShowColorPulse(Color.Purple, duration);
-                ShowColorPulse(Color.Magenta, duration);
-                ShowColorPulse(Color.Pink, duration);
-            }
-        }
-
-        void ShowColorPulse(Color color, int duration = 1000)
-        {
-            onboardLed.StartPulse(color, (int)(duration / 2));
-            Thread.Sleep(duration);
-            onboardLed.Stop();
-        }
-
-        void ShowColor(Color color, int duration = 1000)
-        {
-            Console.WriteLine($"Color: {color}");
-            onboardLed.SetColor(color);
-            Thread.Sleep(duration);
-            onboardLed.Stop();
         }
     }
 }
