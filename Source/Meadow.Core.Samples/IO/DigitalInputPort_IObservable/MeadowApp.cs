c7c8874abf353f830bb6ec93dea107bf045fd105
bryan costanich <b@wowzer.net>
2021-05-21 16:07:15 -0700

    Added observable to digital input sample.

--- Source/Meadow.Core.Samples/IO/DigitalInputPort_IObservable/MeadowApp.cs
+++ Source/Meadow.Core.Samples/IO/DigitalInputPort_IObservable/MeadowApp.cs
@@ -16,36 +16,42 @@ namespace DigitalInputPort_IObservable_Sample
 
         public MeadowApp()
         {
-            // create an input port on D02. 
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            Console.WriteLine("Initializing hardware...");
+
+            //==== create an input port on D02. 
             input = Device.CreateDigitalInputPort(
                 Device.Pins.D02, InterruptMode.EdgeBoth, ResistorMode.InternalPullDown,
                 20, 10);
 
             //==== Classic .NET Events
-            input.Changed += (object sender, DigitalInputPortChangeResult result) => {
-                Console.WriteLine($"Old school event raised; Time: {result.New.Millisecond}, Value: {result.Value}");
+            input.Changed += (object sender, DigitalPortResult result) => {
+                Console.WriteLine($"Old school event raised; Time: {result.New.Time}, Value: {result.New.State}");
             };
 
-            ////===== Filterable Observer
-            //// this illustrates using a FilterableObserver. Note that the filter is an optional
-            //// parameter, if you're interested in all notifications, don't pass a filter/predicate.
-            //// in this case, we filter on events by time, and only notify if the new event is > 1 second from
-            //// the last event. 
-            //_input.Subscribe(new FilterableChangeObserver<DigitalInputPortChangeResult>(
-            //    handler: result => {
-            //        Console.WriteLine($"Observer Observing the Observable, Observably speaking, Time: {result.New.New.Millisecond}");
-            //    },
-            //    // Optional filter paramter, showing a 1 second filter, i.e., only notify
-            //    // if the new event is > 1 second from last.
-            //    filter: result => {
-            //        if (result.Old is { } old) {
-            //            return (result.New.)
-            //        }
-            //        return (f.Delta > new TimeSpan(0, 0, 0, 0, 1000));
-            //    }));
+            //===== Filterable Observer
+            // this illustrates using a FilterableObserver. Note that the filter is an optional
+            // parameter, if you're interested in all notifications, don't pass a filter/predicate.
+            // in this case, we filter on events by time, and only notify if the new event is > 1 second from
+            // the last event.
+            var observer = IDigitalInputPort.CreateObserver(
+                handler: result => {
+                    Console.WriteLine($"Observer Observing the Observable, Observably speaking, Time: {result.New.Time.ToShortTimeString()}");
+                },
+                // Optional filter paramter, showing a 1 second filter, i.e., only notify
+                // if the new event is > 1 second from last.
+                filter: result => {
+                    if (result.Old is { } old) {
+                        return (result.New.Time - old.Time) > TimeSpan.FromSeconds(1);
+                    } else return false;
+                });
+            input.Subscribe(observer);
 
-            Console.WriteLine("Got here 3.");
-
+            Console.WriteLine("Hardware initialized.");
         }
     }
 }
