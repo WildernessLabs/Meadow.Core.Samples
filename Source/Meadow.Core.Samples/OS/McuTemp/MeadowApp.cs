8b777d7ad02f1672ac383e50626858fcba8b6810
bryan costanich <b@wowzer.net>
2021-05-22 19:24:54 -0700

    Add MCU Temp sample

---
+++ Source/Meadow.Core.Samples/OS/McuTemp/MeadowApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+
+namespace MeadowApp
+{
+    /// <summary>
+    /// This sample prints the MCU temp to the console.
+    /// </summary>
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            while(true) {
+                // get the temp
+                Console.WriteLine($"Processor Temp: {Device.GetProcessorTemperature().Celsius:n2}C");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
