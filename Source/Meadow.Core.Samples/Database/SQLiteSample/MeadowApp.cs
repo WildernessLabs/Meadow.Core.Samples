11ef4d712d441030e12a793c504cd6068e091d61
Chris Tacke <christopher.tacke@hypergiant.com>
2021-07-24 13:34:36 -0500

    Starting the SQLite sample/integration work

---
+++ Source/Meadow.Core.Samples/Database/SQLiteSample/MeadowApp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+
+namespace MeadowApp
+{
+    public unsafe class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        public MeadowApp()
+        {
+        }
+
+        public void Run()
+        { 
+            ListFiles(MeadowOS.FileSystem.DataDirectory);
+
+            try
+            {
+                var file = Path.Combine(MeadowOS.FileSystem.DataDirectory, "test.db");
+                file = $"file:{file}";
+                fixed (char* pName = file)
+                {
+                    Console.WriteLine($"Opening DB at {file}...");
+                    var result = NativeMethods.sqlite3_open((byte*)pName, out IntPtr pDB);
+                    Console.WriteLine($"result={result}  DB={pDB}");
+                    if(result != 0)
+                    {
+                        var pMsg = NativeMethods.sqlite3_errmsg(pDB);
+                        Console.WriteLine($"pMsg={(int)pMsg}");
+                        var p = pMsg;
+                        var count = 0;
+                        while(*p != 0)
+                        {
+                            count++;
+                            p++;
+                        }
+                        var message = Encoding.ASCII.GetString(pMsg, count);
+
+                        Console.WriteLine($"errmsg={message}");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"EXCEPTION: {ex.Message}");
+            }
+
+            ListFiles(MeadowOS.FileSystem.DataDirectory);
+        }
+
+        private void ListFiles(string dir)
+        {
+            Console.WriteLine($"Files in {dir}:");
+
+            var files = Directory.GetFiles(dir);
+            if (files == null || files.Length == 0)
+            {
+                Console.WriteLine("  [ EMPTY DIRECTORY ]");
+            }
+            else
+            {
+                foreach (var f in Directory.GetFiles(dir))
+                {
+                    Console.WriteLine($"  {f}");
+                }
+            }
+        }
+    }
+}
---
+++ Source/Meadow.Core.Samples/Database/SQLiteSample/NativeMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MeadowApp
+{
+    public static class NativeMethods
+    {
+        private const string LIB_NAME = "sqlite";
+
+        [DllImport(LIB_NAME)]
+        public static extern unsafe int sqlite3_open(byte* filename, out IntPtr db);
+
+        [DllImport(LIB_NAME)]
+        public static extern unsafe byte* sqlite3_errmsg(IntPtr db);
+    }
+}
