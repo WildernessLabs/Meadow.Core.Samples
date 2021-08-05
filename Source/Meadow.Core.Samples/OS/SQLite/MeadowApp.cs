7d7a1e70c3b971b74f36850d60c8c997fdb00da2
bryan costanich <b@wowzer.net>
2021-08-05 09:12:26 -0700

    Added SQlite sample into the correct branch

---
+++ Source/Meadow.Core.Samples/OS/SQLite/MeadowApp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using SQLite;
+
+namespace MeadowApp
+{
+    public unsafe class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        private int InsertCount = 10;
+
+        public MeadowApp()
+        {
+            SensorValue = 42.42;
+        }
+
+        public double SensorValue { get; set; }
+
+        public void SQLiteNetTest()
+        {
+            var databasePath = Path.Combine(MeadowOS.FileSystem.DataDirectory, "sensor_data.db");
+            var db = new SQLite.SQLiteConnection(databasePath);
+            db.CreateTable<SensorInfo>();
+
+            for (int i = 0; i < InsertCount; i++)
+            {
+                Console.WriteLine($"Inserting row {i + 1}...");
+                db.Insert(new SensorInfo { Timestamp = DateTime.Now, Value = SensorValue });
+                Thread.Sleep(1000);
+                SensorValue += 1.23;
+            }
+
+            Console.WriteLine("Reading back the data...");
+            var rows = db.Table<SensorInfo>();
+            foreach (var r in rows)
+            {
+                Console.WriteLine($"Reading was {r.Value} at {r.Timestamp.ToString("HH:mm:ss")}");
+            }
+        }
+
+        public void ListFiles(string dir)
+        {
+            Console.WriteLine($"Files in {dir}:");
+
+            var files = Directory.GetFiles(dir);
+            if (files == null || files.Length == 0)
+            {
+                Console.WriteLine("  [ EMPTY DIRECTORY ]");
+            }
+            else
+            {
+                foreach (var f in Directory.GetFiles(dir))
+                {
+                    Console.WriteLine($"  {f}");
+                }
+            }
+        }
+    }
+}
---
+++ Source/Meadow.Core.Samples/OS/SQLite/SensorInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MeadowApp
+{
+    public class SensorInfo
+    {
+        public DateTime Timestamp { get; set; }
+        public double Value { get; set; }
+    }
+}
