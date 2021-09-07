bb82155cd13b38ab5e13177423ee482349737fed
bryan costanich <b@wowzer.net>
2021-09-07 11:26:41 -0700

    Added threading config API

--- Source/Meadow.Core.Samples/OS/SQLite/MeadowApp.cs
+++ Source/Meadow.Core.Samples/OS/SQLite/MeadowApp.cs
@@ -53,6 +53,11 @@ namespace MeadowApp
 
         protected void ConfigureDatabase()
         {
+            // by default, SQLite runs in `Serialized` mode, which is thread safe.
+            // if you need to change the threading mode, you can do it with the
+            // following API
+            //SQLite3.Config(SQLite3.ConfigOption.SingleThread);
+
             // database files should go in the `DataDirectory`
             var databasePath = Path.Combine(MeadowOS.FileSystem.DataDirectory, "MySqliteDatabase.db");
             // make the connection
