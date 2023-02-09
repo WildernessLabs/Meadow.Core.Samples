435a2bf36c20d106a128981c6a1e27cf03f53d36
Chris Tacke <ctacke@opennetcf.com>
2023-02-09 10:30:02 -0600

    Meadow.Windows Modbus RTU sample

--- Source/Meadow.Windows.Samples/IO/Keyboard/Program.cs
+++ Source/Meadow.Windows.Samples/IO/Keyboard/Program.cs
@@ -15,15 +15,32 @@ public class MeadowApp : App<Windows>
 
     public override Task Initialize()
     {
-        _keyBoard = new Keyboard();
+        _keyBoard = new Keyboard(1);
         _a = _keyBoard.CreateDigitalInputPort(_keyBoard.Pins.A, InterruptMode.EdgeBoth);
         _a.Changed += OnKeyChanged;
+
         return Task.CompletedTask;
     }
 
     private void OnKeyChanged(object? sender, DigitalPortResult e)
     {
-        var pin = (sender as Keyboard.KeyboardKey)?.Pin as KeyboardPin;
+        var pin = (sender as Keyboard.KeyboardKey)?.Pin as KeyboardKeyPin;
         Debug.WriteLine($"Key '{pin?.Name}' is {(e.New.State ? "down" : "up")}");
+    }
+
+    public override async Task Run()
+    {
+        var capsLock = _keyBoard.Pins.CapsLock.CreateDigitalOutputPort();
+
+        var state = false;
+
+        for (int i = 0; i < 10; i++)
+        {
+            state = !state;
+
+            capsLock.State = state;
+
+            await Task.Delay(1000);
+        }
     }
 }
---
+++ Source/Meadow.Windows.Samples/IO/ModbusRtu/Program.cs
@@ -0,0 +1,44 @@
+﻿using Meadow;
+using Meadow.Foundation.Thermostats;
+using Meadow.Modbus;
+
+public class MeadowApp : App<Windows>
+{
+    public static async Task Main(string[] _)
+    {
+        await MeadowOS.Start();
+    }
+
+    private Tstat8 _thermostat;
+
+    public override Task Initialize()
+    {
+        // use the first one - adjust to your needs
+        var port = Device.CreateSerialPort("COM4", 19200);
+        port.ReadTimeout = TimeSpan.FromSeconds(2);
+        var rtu = new ModbusRtuClient(port);
+        rtu.Connect();
+
+        _thermostat = new Tstat8(rtu, 201);
+
+        return Task.CompletedTask;
+    }
+
+    public override async Task Run()
+    {
+        while (true)
+        {
+            try
+            {
+                var temp = await _thermostat.GetCurrentTemperature();
+
+                Console.WriteLine($"Current temperature: {temp.Fahrenheit}F");
+            }
+            catch
+            {
+            }
+
+            await Task.Delay(1000);
+        }
+    }
+}
