4cb85ed1584e68887b1ad0ad3385337e83090ead
Adam Patridge <patridge@gmail.com>
2023-04-22 19:12:38 -0600

    Update CoreComputeBreakout project to latest API

--- Source/Meadow.Core.Samples/Board_Specific_Samples/CoreComputeBreakout/SPIDisplay.cs
+++ Source/Meadow.Core.Samples/Board_Specific_Samples/CoreComputeBreakout/SPIDisplay.cs
@@ -1,5 +1,5 @@
 ﻿using Meadow.Devices;
-using Meadow.Foundation.Displays.TftSpi;
+using Meadow.Foundation.Displays;
 using Meadow.Foundation.Graphics;
 using Meadow.Hardware;
 using Meadow.Logging;
@@ -14,13 +14,13 @@ namespace MeadowApp
         private IPin _dc;
         private IPin _reset;
 
-        public SPIDisplay(IMeadowDevice device, ISpiBus bus, IPin chipSelect, IPin dc, IPin reset, Logger logger)
+        public SPIDisplay(ISpiBus bus, IPin chipSelect, IPin dc, IPin reset, Logger logger)
             : base(logger, RotationType._90Degrees)
         {
             _chipSelect = chipSelect;
             _dc = dc;
             _reset = reset;
-            _display = new St7789(device, bus, _chipSelect, _dc, _reset, 240, 240);
+            _display = new St7789(bus, _chipSelect, _dc, _reset, 240, 240);
         }
 
         protected override IGraphicsDisplay Display => _display;
