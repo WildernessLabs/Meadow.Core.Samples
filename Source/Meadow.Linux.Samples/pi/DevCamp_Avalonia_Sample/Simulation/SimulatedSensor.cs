1493fa4655d966fa5fe009a693075b38fd2c6a03
Chris Tacke <ctacke@opennetcf.com>
2023-05-08 10:56:53 -0500

    Added starting point for Avalonia sample for DevCamp 2023

---
+++ Source/Meadow.Linux.Samples/pi/DevCamp_Avalonia_Sample/Simulation/SimulatedSensor.cs
@@ -0,0 +1,57 @@
+﻿using Meadow;
+using Meadow.Foundation;
+using System;
+using System.Threading.Tasks;
+
+namespace AvaloniaSample.Simulation
+{
+    public abstract class SimulatedSensor<T> : SamplingSensorBase<T>
+        where T : struct
+    {
+        public event EventHandler<IChangeResult<T>> SensorUpdated = delegate { };
+
+        public T? LastReading { get; private set; } = null;
+
+        protected SimulatedSensor()
+        {
+        }
+
+        protected abstract void RaiseUpdatedEvent(T newValue);
+
+        public override void StartUpdating(TimeSpan? updateInterval = null)
+        {
+            UpdateInterval = updateInterval ?? TimeSpan.FromSeconds(5);
+            var task = new Task(SampleProc, TaskCreationOptions.LongRunning);
+            task.Start();
+        }
+
+        public override void StopUpdating()
+        {
+            SamplingTokenSource?.Cancel();
+        }
+
+        private async void SampleProc()
+        {
+            SamplingTokenSource = new System.Threading.CancellationTokenSource();
+
+            IsSampling = true;
+
+            while (!SamplingTokenSource.IsCancellationRequested)
+            {
+                var value = await ReadSensor();
+
+                //                if (LastReading == null || !value.Equals(LastReading.Value))
+                {
+                    SensorUpdated?.Invoke(this, new ChangeResult<T>(value, LastReading));
+                }
+
+                LastReading = value;
+                await Task.Delay(UpdateInterval);
+            }
+
+            IsSampling = false;
+            SamplingTokenSource.Dispose();
+            SamplingTokenSource = null;
+        }
+    }
+}
---
+++ Source/Meadow.Linux.Samples/pi/DevCamp_Avalonia_Sample/Simulation/SimulatedTempSensor.cs
@@ -0,0 +1,35 @@
+﻿using Meadow;
+using Meadow.Peripherals.Sensors;
+using Meadow.Units;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AvaloniaSample.Simulation
+{
+    public class SimulatedTempSensor : SimulatedSensor<Temperature>, ITemperatureSensor
+    {
+        public Temperature? Temperature => LastReading;
+
+        public event EventHandler<IChangeResult<Temperature>> TemperatureUpdated = delegate { };
+
+        protected override void RaiseUpdatedEvent(Temperature newValue)
+        {
+            TemperatureUpdated?.Invoke(this, new ChangeResult<Temperature>(newValue, LastReading));
+        }
+
+        protected override Task<Temperature> ReadSensor()
+        {
+            lock (base.samplingLock)
+            {
+                var last = LastReading ?? new Temperature(65, Meadow.Units.Temperature.UnitType.Fahrenheit);
+                var delta = Random.Shared.NextSingle();
+                if (Random.Shared.NextSingle() > 0.5f) delta *= -1;
+                var newTemp = new Temperature(last.Fahrenheit + delta, Meadow.Units.Temperature.UnitType.Fahrenheit);
+
+                Debug.WriteLine($"New temperature: {newTemp.Fahrenheit:N1}F");
+                return Task.FromResult(newTemp);
+            }
+        }
+    }
+}
