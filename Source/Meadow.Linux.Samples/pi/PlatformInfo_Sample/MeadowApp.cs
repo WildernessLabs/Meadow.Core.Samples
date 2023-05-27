222bed37849a47406abc9383843a67324471b5d9
Chris Tacke <ctacke@opennetcf.com>
2023-05-27 09:35:45 -0600

    added linux sample for platform info, including processor temp

---
+++ Source/Meadow.Linux.Samples/pi/PlatformInfo_Sample/MeadowApp.cs
@@ -0,0 +1,31 @@
+﻿using Meadow;
+using Meadow.Pinouts;
+using System;
+using System.Threading.Tasks;
+
+namespace PushButton_Sample
+{
+    public class MeadowApp : App<Linux<RaspberryPi>>
+    {
+        public static async Task Main(string[] args)
+        {
+            await MeadowOS.Start(args);
+        }
+
+        public override async Task Run()
+        {
+
+            Console.WriteLine($"Platform:    {Device.Information.Platform}");
+            Console.WriteLine($"Device Name: {Device.Information.DeviceName}");
+            Console.WriteLine($"Model:       {Device.Information.Model}");
+            Console.WriteLine($"OS version:  {Device.PlatformOS.OSVersion}");
+
+            while (true)
+            {
+                Console.Write($"CPU Temp: {Device.PlatformOS.GetCpuTemperature().Celsius:N2}C    \r");
+                await Task.Delay(1000);
+            }
+        }
+
+    }
+}
