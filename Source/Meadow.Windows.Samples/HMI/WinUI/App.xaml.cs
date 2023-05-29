7cfbc6e1aeb8317fa70dac71083b60cac31f1678
Chris Tacke <ctacke@opennetcf.com>
2023-05-29 09:35:12 -0600

    Added initial WinUI sample structure

---
+++ Source/Meadow.Windows.Samples/HMI/WinUI/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Meadow;
+using Microsoft.UI.Xaml;
+
+namespace WinUIMeadow
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : WinUIMeadowApplication
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.LoadMeadowOS();
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            m_window.Activate();
+            // add this to the resolver so the app can use it for invoking
+            Resolver.Services.Add<Window>(m_window);
+        }
+
+        private Window m_window;
+    }
+}
---
+++ Source/Meadow.Windows.Samples/HMI/WinUI/WinUIMeadowApplication.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Meadow;
+
+public class WinUIMeadowApplication : Application, IApp
+{
+    public CancellationToken CancellationToken => throw new NotImplementedException();
+
+    public static Windows Device => Resolver.Services.Get<IMeadowDevice>() as Windows;
+
+    protected WinUIMeadowApplication()
+    {
+    }
+
+    public void InvokeOnMainThread(Action<object?> action, object? state = null)
+    {
+        var mainWindow = Resolver.Services.Get<Window>();
+
+        if (mainWindow == null)
+        {
+            throw new Exception("Main Windows was not added to the resolver.  Add it in App.xaml.cs");
+        }
+        mainWindow.DispatcherQueue.TryEnqueue(() => action(state));
+    }
+
+    virtual public Task OnError(Exception e)
+    {
+        return Task.CompletedTask;
+    }
+
+    virtual public Task OnShutdown()
+    {
+        return Task.CompletedTask;
+    }
+
+    virtual public void OnUpdate(Version newVersion, out bool approveUpdate)
+    {
+        approveUpdate = true;
+    }
+
+    virtual public void OnUpdateComplete(Version oldVersion, out bool rollbackUpdate)
+    {
+        rollbackUpdate = false;
+    }
+
+    virtual public Task Run()
+    {
+        return Task.CompletedTask;
+    }
+
+    public virtual Task InitializeMeadow()
+    {
+        return Task.CompletedTask;
+    }
+
+    Task IApp.Initialize()
+    {
+        return InitializeMeadow();
+    }
+
+    protected void LoadMeadowOS()
+    {
+        new Thread((o) =>
+        {
+            _ = MeadowOS.Start(this, null);
+        })
+        {
+            IsBackground = true
+        }
+        .Start();
+    }
+}
